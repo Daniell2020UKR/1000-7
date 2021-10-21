e1739c7b87235bf23dfd108b725e9e911633ee85
Daniell2020UKR <66268402+Daniell2020UKR@users.noreply.github.com>
2021-10-21 22:10:25 +0300

    Add files via upload

---
+++ 1000-7.py
@@ -0,0 +1,17 @@
+﻿import io, inspect
+from .. import loader, utils
+from asyncio import sleep
+
+@loader.tds
+class GULMod(loader.Module):
+    """Я — гуль!"""
+    strings = {'name': '1000-7'}
+    
+    async def гульcmd(self, message):
+        await message.delete()
+        x = 1000
+        await sleep(0.5)
+        while x >0:
+            await message.client.send_message(message.to_id, str(x) + " - 7 = " + str(x-7))
+            x -= 7
+            await sleep(0.001)
